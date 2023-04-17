ba410bbec410470fbcc277d3b3988c9f8743f2b8
Valentin Da silva <v.dasilva11d@gmail.com>
2023-04-17 22:25:01 +0200

    added ex05

---
+++ lib/05_timer.rb
@@ -0,0 +1,9 @@
+#05_timer
+
+def time_string(wanted_time)
+  minutes = wanted_time/60
+  hours = minutes/60
+  seconds = wanted_time -(minutes*60)
+  minutes = minutes - (hours*60)
+  return "#{'%02d'%hours}:#{'%02d'%minutes}:#{'%02d'%seconds}"
+end
